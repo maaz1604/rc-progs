f67a5a9f8c1cfeb95b2dfa4386bc52a773d50697
Maaz Amir <amirmaaz16@gmail.com>
2024-10-20 21:35:48 +0530

    tailwind css code with react is added

---
+++ tail_props/src/App.jsx
@@ -0,0 +1,42 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Card from "./components/cards";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-lg">Tailwind tests</h1>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="https://images.pexels.com/photos/12918397/pexels-photo-12918397.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I have seen scale on
+              large teams. It’s easy to customize, adapts to any design, and the
+              build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+      <br />
+
+      <Card username = "Diable" price={586} />
+    </>
+  );
+}
+
+export default App;
