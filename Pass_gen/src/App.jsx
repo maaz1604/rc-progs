1597e06740ebf1dc3899a961ff14e6421fc5f163
Maaz Amir <amirmaaz16@gmail.com>
2024-10-22 12:12:41 +0530

    Password Generator project added

---
+++ Pass_gen/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState, useCallback,useEffect,useRef } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  const [Length, setLength] = useState(8);
+  const [numberAllowed, setNumber_allowed] = useState(false);
+  const [characterAllowed, setCharacter_allowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //Useref hook
+  const passRef = useRef(null);
+
+  const passGenerator = useCallback(() => {
+    let Pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (characterAllowed) {
+      str += "!@#$&*~";
+    }
+    for (let i = 0; i <= Length; i++) {
+      let char = Math.floor(Math.random()*str.length+1);
+      Pass += str.charAt(char);
+    }
+
+    setPassword(Pass);
+
+
+  }, [Length, numberAllowed, characterAllowed, setPassword]);
+
+  const copyPass_onclipBoard = useCallback(() => {
+    passRef.current?.select();
+    passRef.current?.setSelectionRange(0,999);
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
+  useEffect(() => {
+    passGenerator();
+  },[Length,numberAllowed,characterAllowed,passGenerator])
+
+  return (
+    <>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-400 bg-gray-700">
+    <h1 className="text-white text-center my-3">Password Generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+    <input type="text" value={password} className="outline-none w-full py-1 px-3" placeholder="passwords" readOnly ref={passRef}/>
+    <button
+    onClick={copyPass_onclipBoard}
+    className="outline-none hover:cursor-pointer bg-blue-500 text-white px-3 py-0.5 shrink-0">
+      Copy
+    </button>
+    </div>
+    <div className="flex text-sm gap-x-2">
+    <div className="flex items-center gap-x-1"></div>
+    <input type="range" 
+    min={6} max={100}
+    value={Length}
+    className="cursor-pointer"
+    onChange={(e) => {setLength(e.target.value)}}
+    />  
+    <label >Length: {Length}</label>
+    <div className="flex items-center gap-x-1">
+    <input type="checkbox"
+    defaultChecked={numberAllowed}
+    id="numberInput"
+    onChange={() => {
+      setNumber_allowed((prev) => !prev);
+    }}
+    />
+    <label htmlFor="numberInput">Numbers</label>
+    </div>
+    <div className="flex items-center gap-x-1">
+    <input type="checkbox"
+    defaultChecked={characterAllowed}
+    id="characterInput"
+    onChange={() => {
+      setCharacter_allowed((prev) => !prev);
+    }}
+    />
+    <label htmlFor="characterInput">Characters</label>
+    </div>
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-Test
+All the Credit of this repo goes to Hitesh Choudhary
+
+Hitesh sir email id - hitesh@hiteshchoudhary.com
+Hitesh sir official insta - hiteshchoudharyofficial
+Hitesh sir linkedin - https://www.linkedin.com/in/hiteshchoudhary?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BIWB6CIlFSeWH49uNwpI4sw%3D%3D
